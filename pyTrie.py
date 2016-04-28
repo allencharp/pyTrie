c84d02db7439b4ac396d890398ab826728b9a758
allen <liuzhidong_1985@hotmail.com>
2016-04-28 22:39:16 +0800

    init commit for TireNode class

--- pyTrie.py
+++ pyTrie.py
@@ -1,14 +1,18 @@
 class TrieNode:
-	def __init__(self):
-		self.alph = ""
-		self.next = []
-		pass
-
-	def walk(self):
+	def __init__(self, parent, key, nodes):
+		self.parent = None # TrieNode value
+		self.key = "" # char/string value
+		self.nodes = [] # [TrieNode, TrieNode, ....]
 		pass
 
 	def path(self):
-		pass
+		rtn = ""
+		n = self
+		while n.parent is not None:
+			rtn += (n.key)
+			n = n.parent
+
+		return rtn[::-1] #reserve a string
 
 class PyTrie:
 	def loadData(self):
@@ -18,3 +22,10 @@ class PyTrie:
 	def getPrefixData(self, prefix):
 		pass
 
+
+print ("aaa"+"bbb")[::-1]
+
+a = iter(list(range(10)))
+for i in a:
+	print(i)
+	next(a)
