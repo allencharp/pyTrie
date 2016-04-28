94052b7869d4c45cca0c9ed9ca2d325d55afe37c
allen <liuzhidong_1985@hotmail.com>
2016-04-28 22:03:51 +0800

    init

--- pyTrie.py
+++ pyTrie.py
@@ -1,2 +1,20 @@
+class TrieNode:
+	def __init__(self):
+		self.alph = ""
+		self.next = []
+		pass
+
+	def walk(self):
+		pass
+
+	def path(self):
+		pass
+
 class PyTrie:
-	
+	def loadData(self):
+
+		pass
+
+	def getPrefixData(self, prefix):
+		pass
+
