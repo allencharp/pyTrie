2d54a28812ba7d2651a5dd5a8cd7f61028a7a3dc
allen <liuzhidong_1985@hotmail.com>
2016-04-30 19:00:16 +0800

    pyTrie add function

--- pyTrie.py
+++ pyTrie.py
@@ -4,19 +4,19 @@ class TrieNode:
 	def __init__(self, parent = None, key = None):
 		self.parent = parent    # TrieNode value
 		self.key = key          # char/string value
-		self.nodes = None       # [TrieNode, TrieNode, ....]
+		self.nodes = []       # [TrieNode, TrieNode, ....]
 		pass
-	
+
 	# the way to yield the tree node with recursive way....
 	def __iter__(self):
-		if self.nodes != None:
+		if len(self.nodes) != 0:
 			for node in self.nodes:
 				for n in node:
 					yield n
 		yield self
 
-	def setchildren(self, nodes):
-		self.nodes = nodes
+	def addchild(self, nodes):
+		self.nodes.append(nodes)
 
 	@property
 	def path(self):
@@ -28,13 +28,34 @@ class TrieNode:
 		return rtn[::-1]  # reserve a string
 
 
+class PyTrie(object):
+	_instance = None
+	root = None
 
-class PyTrie:
-	def __init__(self):
-		pass
+	# singleton
+	def __new__(cls, *args, **kwargs):
+		if not cls._instance:
+			cls._instance = super(PyTrie, cls).__new__(
+				cls, *args, **kwargs)
+			cls._instance.root = TrieNode(parent=None, key="")
+		return cls._instance
 
 	def add(self, str):
-		pass
+		cur_node = self.root
+		for loc, key in enumerate(str):
+			node = self.__exist(cur_node, key)
+			if node is not None:
+				cur_node = node
+			else:
+				new_child = TrieNode(cur_node, key)
+				cur_node.addchild(new_child)
+				cur_node = new_child
+
+	def __exist(self, trie_node, s):
+		for n in trie_node.nodes:
+			if n.key == s:
+				return n
+		return None
 
 	def delete(self, str):
 		pass
--- test_pyTrie.py
+++ test_pyTrie.py
@@ -1,23 +1,33 @@
 from pyTrie import  TrieNode
-
-
-
+from pyTrie import PyTrie
 
 rootnode = TrieNode(None, '')
 anode = TrieNode(rootnode,'a')
 bnode = TrieNode(rootnode,'b')
-rootnode.setchildren([anode, bnode])
+rootnode.addchild(anode)
+rootnode.addchild(bnode)
 
 acnode = TrieNode(anode, 'c')
-anode.setchildren([acnode])
+anode.addchild(acnode)
 
 accnode = TrieNode(acnode, 'c')
-acnode.setchildren([accnode])
+acnode.addchild(accnode)
 
 bdnode = TrieNode(bnode,'d')
-bnode.setchildren([bdnode])
+bnode.addchild(bdnode)
 
-for i in rootnode:
-	print i.path
+def test_trienode():
+	for i in rootnode:
+		print i.path
 
-print acnode.path
+def test_pytrie_add():
+	pytrie = PyTrie()
+	pytrie.add("abc")
+	print pytrie.root.nodes[0].nodes[0].nodes[0].key
+	pytrie.add("abd")
+	print pytrie.root.nodes[0].nodes[0].nodes[1].key
+
+
+#test_trienode()
+test_pytrie_add()
+
